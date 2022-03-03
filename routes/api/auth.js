37b24d5d486088a114dd17dbe89c29334be55e5e
Vivek79781 <vivekdbz248@gmail.com>
2022-03-03 18:04:32 +0530

    initial commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const router = express.Router();
-
+const auth = require("../../middleware/auth");
+const { findById } = require('../../models/user');
+const User = require("../../models/user")
 
 // @route       GET api/auth
 // @desc        Test Route
 // @access      Public
-router.get('/',(req,res) => {
-    res.send('Auth Route Working')
+router.get('/', auth, async(req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user);
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send('Server Error')
+    }
 })
 
 module.exports = router;

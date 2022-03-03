cf0bcae9ac529108df1d9f19796463e0b22a591c
Vivek79781 <vivekdbz248@gmail.com>
2022-03-03 11:04:09 +0530

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const connectDB = require('./config/db')
+
 const app = express();
+
+connectDB();
 
 app.get('/',(req,res) => {
     res.send("WORKING");
 })
+
+
+// Routes
+app.use('/api/users',require('./routes/api/user'))
+app.use('/api/auth',require('./routes/api/auth'))
+app.use('/api/profile',require('./routes/api/profile'))
+app.use('/api/posts',require('./routes/api/post'))
+
+
 const port = process.env.process || 3000
 
 app.listen(port, () => {

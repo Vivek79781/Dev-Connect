33b8be70335c60f3ef2f001defab4ead2365f9cb
Vivek79781 <vivekdbz248@gmail.com>
2022-06-06 20:40:20 +0530

    Done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const connectDB = require("./config/db")
 const cors = require('cors')
 const app = express()
+const path = require('path')
 
 //Connect to database
 connectDB()
@@ -12,15 +13,20 @@ const port = process.env.PORT || 3030
 app.use(express.json({extended: false}))
 app.use(cors())
 
-app.get("/",(req,res)=>{
-    res.send("API running")
-})
 
 app.use('/api/users', require('./routes/api/user.js'))
 app.use('/api/auth', require('./routes/api/auth.js'))
 app.use('/api/profile', require('./routes/api/profile.js'))
 app.use('/api/posts', require('./routes/api/post.js'))
 
+// Serve Static Assets for production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'))
+    app.use('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
